7e0c035babc6c99f975b2207a061915a2080c736
Marko Zecevic <markic2001@hotmail.de>
2023-12-13 17:46:56 +0100

    Finished Langevin exercise.

--- sm1_worksheet_4/solutions/sim.py
+++ sm1_worksheet_4/solutions/sim.py
@@ -50,64 +50,44 @@ def step_vv(x, v, f, dt):
 
     return x, v, f
 
-def draw_random_force_(f, dt, gamma):
+def draw_random_gaussian_force(f, dt, gamma):
     random_vector = np.random.normal(0.0, np.sqrt(2*T*gamma/dt), f.shape)
     return f + random_vector
 
-def draw_random_force(f: np.ndarray, dt: float, gamma: float) -> np.ndarray:
+def draw_random_uniform_force(f: np.ndarray, dt: float, gamma: float) -> np.ndarray:
     wanted_std = np.sqrt(2*gamma*T/dt)
 
-    random_vector = np.random.random(f.shape)
-    random_vector -= np.mean(random_vector, axis=0)
-    random_vector *= wanted_std / np.std(random_vector, axis=0)
-
-    assert random_vector.shape == f.shape
-    assert np.allclose(np.mean(random_vector, axis=0), np.zeros(3))
-    assert np.allclose(np.std(random_vector, axis=0), np.full((3), wanted_std))
-
-    # plt.hist(random_vector, bins=20)
-    # plt.show()
-
+    random_vector = np.random.uniform(low=-np.sqrt(3)*wanted_std, high=np.sqrt(3)*wanted_std, size=f.shape)
     return f + random_vector
 
-# def test_random_numbers(gamma, dt):
-#     wanted_std = np.sqrt(2*gamma*T/dt)
+def test_uniform_vectors(dt, gamma):
+    random_test_vector = draw_random_uniform_force(np.zeros((1000000, 3)), dt, gamma)
 
-#     test_arr = np.random.uniform(size=1000)
-#     test_arr -= np.mean(test_arr, axis=0)
-#     plt.hist(test_arr, bins=20)
-#     plt.show()
-#     test_arr *= wanted_std / np.std(test_arr, axis=0)
+    assert np.allclose(np.mean(random_test_vector, axis=0), np.zeros(3), atol=0.05)
+    assert np.allclose(np.std(random_test_vector, axis=0), np.full((3), np.sqrt(2*gamma*T/dt)), atol=0.05)
 
-#     plt.hist(test_arr, bins=20)
-#     plt.show()
 
-#     print(np.mean(test_arr))
-#     print(np.std(test_arr), wanted_std)
+test_uniform_vectors(DT, GAMMA_LANGEVIN)
 
-# test_random_numbers(GAMMA_LANGEVIN, DT)
 
 # used T=k=1
-def step_vv_langevin(x, v, f, dt, gamma):
-    # draw random force G
-    g = draw_random_force_(f, dt, gamma)    
-
+def step_vv_langevin(x, v, g, dt, gamma):
     # update positions
     x += v * dt * (1 - 0.5* dt * gamma) + 0.5 * g * dt * dt
 
     # half update of the velocity
-    v = v * (1 - 0.5*dt*gamma) + 0.5 * dt * g
-    v /= (1 + 0.5* dt * gamma) 
+    # v = v * (1 - 0.5*dt*gamma) + 0.5 * dt * g
+    # v /= (1 + 0.5* dt * gamma) 
+    v = (v * (1 - 0.5 * dt * gamma) + 0.5 * dt * g) / (1 + 0.5 * dt * gamma)
 
     # for this excercise no forces from other particles
     f = np.zeros_like(x)
-    g = draw_random_force_(f, dt, gamma)
+    g = draw_random_uniform_force(f, dt, gamma)
 
     # second half update of the velocity
-    v = v * (1 - 0.5*dt*gamma) + 0.5 * dt * g
-    v /= (1 + 0.5* dt * gamma) 
+    v += (0.5 * dt * g) / (1 + 0.5 * dt * gamma)
     
-    return x, v, f
+    return x, v, g
 
 
 # SET UP SYSTEM OR LOAD IT
@@ -127,10 +107,8 @@ vels = []
 vels_ = []
 traj = []
 
-
 # main loop
 f = np.zeros_like(x)
-
 
 print(f"Simulating until tmax={TIME_MAX}...")
 
@@ -143,9 +121,8 @@ while t < TIME_MAX:
     if step % MEASUREMENT_STRIDE == 0:
         E = compute_energy(v)
         Tm = compute_temperature(v)
-        vels.append(v.flatten())
-        traj.append(x.flatten())
-        vels_.append(v)
+        vels.append(v) # changed the flatten
+        traj.append(x)
         # print(f"t={t}, E={E}, T_m={Tm}")
 
         ts.append(t)
@@ -154,7 +131,7 @@ while t < TIME_MAX:
 
 
 # at the end of the simulation, write out the final state
-datafilename = f'{args.id}.dat.gz'
+datafilename = f'./sm1_worksheet_4/plots/{args.id}.dat.gz'
 print(f"Writing simulation data to {datafilename}.")
 vels = np.array(vels)
 traj = np.array(traj)
@@ -166,31 +143,44 @@ datafile.close()
 print("Finished simulation.")
 print("Computing plots ...")
 
-plt.plot(ts, Tms)
-plt.show()
-
-def compute_quadratically_averaged_velocities(vels: np.ndarray) -> np.ndarray:
+def running_average(O: np.ndarray, M: int) -> np.ndarray:
     """
-    vels: velocity vectors of all particles. shape: (timesteps, particles, dimensions)
+    Computes the running average of the np.array O with M being the half-window-size.
+    
+    Currently only works for 1d arrays. Could be become an issue.
     """
-    averaged_vector = np.linalg.norm(
-                vels, axis=2,
-    )
-    return averaged_vector
+    array_length = len(O)
+    output = np.full(array_length, np.nan)
+    for i in range(M, array_length - M):
+        output[i] = np.sum(O[i - M: i + M + 1], axis=0) / (2*M + 1)
+    
+    return output
 
 def maxwell_boltzmann(vel: np.ndarray, temp: float):
-    prefactor = 4*np.pi*np.power(2*np.pi*temp, 2/3)
+    prefactor = np.sqrt(2/np.pi)*np.power(1/temp, 3/2)
     exponential = np.exp(- (np.dot(vel, vel)) / (2*temp))
     return prefactor*np.dot(vel, vel)*exponential
 
-vels_hist = compute_quadratically_averaged_velocities(np.asarray(vels_))
+# Temperature plot
+plt.plot(ts, Tms, label='Temperature trajectory')
+plt.plot(ts, running_average(np.asarray(Tms), 100), label='running avetage with M=100')
+plt.legend()
+plt.xlabel(f'Timesteps')
+plt.ylabel(r'Temperature $T$')
+plt.savefig('./sm1_worksheet_4/plots/langevin_temperature_plot.png', format='png', dpi=150)
+plt.show()
 
-plot_velocities = np.arange(0.0, 4.0, 0.01)
+vels_hist = np.linalg.norm(np.asarray(vels), axis=2,)
+
+plot_velocities = np.arange(0.0, 3.0, 0.01)
 maxwell_velocities = [np.array([v, 0.0, 0.0]) for v in plot_velocities]
 plot_maxwell = [maxwell_boltzmann(vel, T) for vel in maxwell_velocities]
-plt.hist(vels_hist[:, 0], bins=20, density=True)
+plt.hist(vels_hist[:, 0], bins=25, density=True, label='velocity histogram')
 plt.plot(
-    plot_velocities, plot_maxwell, label='was passiert hier'
+    plot_velocities, plot_maxwell, label='Maxwell-Boltzmann distribution'
 )
 plt.legend()
+plt.xlabel(r'Velocity $| \bf{v} |$')
+plt.ylabel(r'Probability $P(|\bf{v}|)$')
+plt.savefig('./sm1_worksheet_4/plots/Maxwell_Boltzmann_distribution.png', format='png', dpi=150)
 plt.show()
